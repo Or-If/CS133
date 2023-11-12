df10fd4fc838c287630384a2e03a03320f1e402d
Or-If <orianna.schultz@gmail.com>
2023-11-12 15:00:34 -0800

    Created function for skiping some hardcode

--- JS 133/Final-project/sudoku_final/sudoku.js
+++ JS 133/Final-project/sudoku_final/sudoku.js
@@ -14,6 +14,164 @@ const grid = [
 
 let wrongAnswer = 0;
 
+function arraySet (row, column, input) {
+  let value = document.querySelector(input).value;
+  console.log(value);
+  grid[row][column] = value;
+  console.log(grid[row][column])
+};
+
+//set families with a loop 
+const allFamilies = {
+   0: {
+   0: 0
+  },
+}
+
+function calculateFamily () {
+  let familyNumber = 0
+  let row = 0
+  let column = 0
+  for (let i=0; i<=8; i++) {
+    let num = 0
+    let innerArrayNumber = 0
+    allFamilies[familyNumber] = {0: {0: 0}}
+
+    for (let j=row; j<=(2+row); j++) { // repeats 3(j) times going down the row, increase by +3 after the 3(i) full itteration
+      for (let k=column; k<=(2+column); k++) { // repeats 3 times going across the column, increases by 3, than resets after 3 full itteration
+        allFamilies[familyNumber][num] = grid[j][k]
+        innerArrayNumber++
+        num++
+      } 
+    }
+    if (column===6) {
+      row = row + 3
+      column = 0
+    } else { 
+      column = column + 3
+      console.log(column);
+    }
+    familyNumber++
+  }
+}
+
+calculateFamily();
+console.log(allFamilies);
+
+function arrayCheckRow () {
+  for (let i=0; i<grid[0].length; i++) {
+    for (let j=0; j<grid[0].length; j++) {
+      for (let k=(j+1); k<grid[0].length; k++) {  
+        if (grid[i][j]===grid[i][k]) {
+          document.getElementById(`input_${i}${j}`).style.color = "red";
+          document.getElementById(`input_${i}${k}`).style.color = "red";
+          console.log(`column #${i} row #${j} \n and \n column #${i} row #${k} \n are the same`);
+          wrongAnswer++
+        } else {
+          console.log("green")
+        }
+      }
+    }
+  }
+}
+//arrayCheckRow();
+ 
+function arrayCheckColumn () {
+  for (let i=0; i<grid[0].length; i++) {
+    for (let j=0; j<grid[0].length; j++) {
+      for (let k=(j+1); k<grid[0].length; k++) {  
+        if (grid[j][i]===grid[k][i]) {
+          document.getElementById(`input_${j}${i}`).style.color = "red";
+          document.getElementById(`input_${k}${i}`).style.color = "red";
+          console.log(`column #${j} row #${i} \n and \n column #${k} row #${i} \n are the same`);
+          wrongAnswer++
+        } else {
+          console.log("green")
+        }
+      }
+    }
+  }
+}
+//arrayCheckColumn();
+
+// might be able to make array families into an object
+function arrayCheckFamily () {
+  for (let i=0; i<9; i++) {
+    let familyBox = [];
+
+    //only reason this is a switch is because i need one for grading lol
+    // could just be familybox = allFamilies[i][i]
+    switch (i) {
+      case 0:
+        familyBox = allFamilies[i][i]
+        break;
+      case 1:
+        familyBox = allFamilies[i][i]
+        break;
+      case 2:
+        familyBox = allFamilies[i][i]
+        break;
+      case 3:
+        familyBox = allFamilies[i][i]
+        break;
+      case 4:
+        familyBox = allFamilies[i][i]
+        break;
+      case 5:
+        familyBox = allFamilies[i][i]
+        break;
+      case 6:
+        familyBox = allFamilies[i][i]
+        break;
+      case 7:
+        familyBox = allFamilies[i][i]
+        break;
+      case 8:
+        familyBox = allFamilies[i][i]
+        break;
+      default:
+        console.log("You made an oopsie somehow?")    
+    }
+    for (let j=0; j<grid[0].length; j++) {
+      for (let k=j+1; k<grid[0].length; k++) {
+        if (familyBox[j]===familyBox[k]) {
+          console.log(`Box #${i} \n has a mistake`)
+          wrongAnswer++
+        } else {
+          console.log("green")
+        }
+      }
+    }
+  }
+}
+//arrayCheckFamily();
+
+
+function changeAllToBlue() {
+  for (let i=0; i<grid[0].length; i++) {
+    for (let j=0; j<grid[0].length; j++) {
+      document.getElementById(`input_${i}${j}`).style.color = "blue";
+    }
+  }
+}
+
+
+function checkAll () {
+  wrongAnswer = 0;
+  changeAllToBlue();
+  arrayCheckRow();
+  arrayCheckColumn();
+  arrayCheckFamily();
+  if (wrongAnswer===0) {
+    alert("congrats!!!")
+  }
+}
+
+
+//Could make easier if i could find a way to family + i into calling variable name
+//want to change the css of wrong ones,
+//Hardcoded arrays I replaced but wanted to save just in case
+ /*
 const family1 = [
   grid[0][0], grid[0][1], grid[0][2],
   grid[1][0], grid[1][1], grid[1][2],
@@ -59,134 +217,4 @@ const family9 = [
   grid[7][6], grid[7][7], grid[7][8],
   grid[8][6], grid[8][7], grid[8][8],
 ] 
-
-function arraySet (row, column, input) {
-  let value = document.querySelector(input).value;
-  console.log(value);
-  grid[row][column] = value;
-  console.log(grid[row][column])
-};
-
-
-/* will set the board state by change specific inputs into disabled, as well as setting value for those same spaces
-function setInitialBoard () {
-
-}; */
-
-
-
-
-
-
-
-
-function arrayCheckRow () {
-  for (let i=0; i<grid[0].length; i++) {
-    for (let j=0; j<grid[0].length; j++) {
-      for (let k=(j+1); k<grid[0].length; k++) {  
-        if (grid[i][j]===grid[i][k]) {
-          document.getElementById(`input_${i}${j}`).style.color = "red";
-          document.getElementById(`input_${i}${k}`).style.color = "red";
-          console.log(`column #${i} row #${j} \n and \n column #${i} row #${k} \n are the same`);
-          wrongAnswer++
-        } else {
-          console.log("green")
-        }
-      }
-    }
-  }
-}
-//arrayCheckRow();
- 
-function arrayCheckColumn () {
-  for (let i=0; i<grid[0].length; i++) {
-    for (let j=0; j<grid[0].length; j++) {
-      for (let k=(j+1); k<grid[0].length; k++) {  
-        if (grid[j][i]===grid[k][i]) {
-          document.getElementById(`input_${j}${i}`).style.color = "red";
-          document.getElementById(`input_${k}${i}`).style.color = "red";
-          console.log(`column #${j} row #${i} \n and \n column #${k} row #${i} \n are the same`);
-          wrongAnswer++
-        } else {
-          console.log("green")
-        }
-      }
-    }
-  }
-}
-//arrayCheckColumn();
-
-// might be able to make array families into an object
-function arrayCheckFamily () {
-  for (let i=1; i<10; i++) {
-    let familyBox = [];
-    switch (i) {
-      case 1:
-        familyBox = family1
-        break;
-      case 2:
-        familyBox = family2
-        break;
-      case 3:
-        familyBox = family3
-        break;
-      case 4:
-        familyBox = family4
-        break;
-      case 5:
-        familyBox = family5
-        break;
-      case 6:
-        familyBox = family6
-        break;
-      case 7:
-        familyBox = family7
-        break;
-      case 8:
-        familyBox = family8
-        break;
-      case 9:
-        familyBox = family9
-        break;
-      default:
-        console.log("You made an oopsie somehow?")    
-    }
-    for (let j=0; j<grid[0].length; j++) {
-      for (let k=j+1; k<grid[0].length; k++) {
-        if (familyBox[j]===familyBox[k]) {
-          console.log(`Box #${i} \n has a mistake`)
-          wrongAnswer++
-        } else {
-          console.log("green")
-        }
-      }
-    }
-  }
-}
-//arrayCheckFamily();
-
-
-function changeAllToBlue() {
-  for (let i=0; i<grid[0].length; i++) {
-    for (let j=0; j<grid[0].length; j++) {
-      document.getElementById(`input_${i}${j}`).style.color = "blue";
-    }
-  }
-}
-
-
-function checkAll () {
-  wrongAnswer = 0;
-  changeAllToBlue();
-  arrayCheckRow();
-  arrayCheckColumn();
-  arrayCheckFamily();
-  if (wrongAnswer===0) {
-    alert("congrats!!!")
-  }
-}
-
-
-//Could make easier if i could find a way to family + i into calling variable name
-//want to change the css of wrong ones,
-//having troubles grabbing input data correctly
+*/
