03eb5b2ee87473dc533e4d984fe441c1ab82d7b5
Or-If <143971808+Or-If@users.noreply.github.com>
2023-11-26 18:51:12 -0800

    added function for generating the puzzle

--- JS 133/Final-project/sudoku_final/sudoku.js
+++ JS 133/Final-project/sudoku_final/sudoku.js
@@ -1,25 +1,46 @@
 
 const grid = [
-  ["1", "1", "1", "4", "1", "6", "1", "1", "2"],
-  ["8", "1", "1", "1", "5", "3", "1", "9", "1"],
-  ["9", "1", "6", "1", "1", "8", "1", "1", "1"],
-  ["6", "7", "1", "1", "8", "1", "1", "1", "9"],
-  ["1", "1", "1", "1", "1", "1", "3", "7", "1"],
-  ["1", "1", "8", "1", "1", "1", "5", "1", "1"],
-  ["1", "8", "1", "1", "4", "1", "2", "5", "1"],
-  ["1", "1", "1", "1", "1", "1", "1", "1", "4"],
-  ["4", "1", "9", "1", "1", "1", "6", "1", "1"]
+  [NaN, NaN, NaN, "4", NaN, "6", NaN, NaN, "2"],
+  ["8", NaN, NaN, NaN, "5", "3", "1", "9", NaN],
+  ["9", NaN, "6", NaN, NaN, "8", NaN, NaN, NaN],
+  ["6", "7", NaN, "1", "8", NaN, NaN, NaN, "9"],
+  ["1", NaN, NaN, NaN, NaN, NaN, "3", "7", NaN],
+  [NaN, NaN, "8", NaN, NaN, NaN, "5", NaN, NaN],
+  [NaN, "8", NaN, NaN, "4", NaN, "2", "5", NaN],
+  [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, "4"],
+  ["4", NaN, "9", NaN, NaN, NaN, "6", "1", NaN]
 ]
 //console.log(grid);
 
-let wrongAnswer = 0;
+let wrongAnswer = 0; 
 
+//sets difficulty that user can change through buttons
+function setGrid (difficulty) {
+ // Use an API to collect my grids from a google doc
+}
+
+//changes internal values of html inputs to the preset puzzles
+function setPuzzle () {
+  for (let i=0; i<grid[0]; i++) {
+    for (let j=0; j<grid[0]; j++) {
+      if (grid[i][j] != NaN) {
+        document.getElementById(`input_${i}${j}`).value = grid[i][j]
+      }
+    }  
+  }
+}
+
+function callPuzzleWithDifficulty () {
+  //this will be responsible for changing the puzzle based on the difficulty you choose, I have to learn about api's to implement
+}
+
+//changes the grid object to match what is being entered
 function arraySet (row, column, input) {
   let value = document.querySelector(input).value;
   console.log(value);
   grid[row][column] = value;
   console.log(grid[row][column])
-};
+};  
 
 //set families with a loop 
 const allFamilies = {
@@ -28,6 +49,7 @@ const allFamilies = {
   },
 }
 
+//creates "families" the 3x3 cubes that are checked for sudoku
 function calculateFamily () {
   let familyNumber = 0
   let row = 0
@@ -58,6 +80,7 @@ function calculateFamily () {
 calculateFamily();
 console.log(allFamilies);
 
+//checks array to see if anything matches in rows
 function arrayCheckRow () {
   for (let i=0; i<grid[0].length; i++) {
     for (let j=0; j<grid[0].length; j++) {
@@ -76,6 +99,7 @@ function arrayCheckRow () {
 }
 //arrayCheckRow();
  
+//checks array to see if anything matches in column
 function arrayCheckColumn () {
   for (let i=0; i<grid[0].length; i++) {
     for (let j=0; j<grid[0].length; j++) {
@@ -94,7 +118,7 @@ function arrayCheckColumn () {
 }
 //arrayCheckColumn();
 
-// might be able to make array families into an object
+//checks array to see if anything matches within families
 function arrayCheckFamily () {
   for (let i=0; i<9; i++) {
     let familyBox = [];
@@ -130,7 +154,7 @@ function arrayCheckFamily () {
         familyBox = allFamilies[i][i]
         break;
       default:
-        console.log("You made an oopsie somehow?")    
+        console.log("An error has occured in arrayCheckFamily")    
     }
     for (let j=0; j<grid[0].length; j++) {
       for (let k=j+1; k<grid[0].length; k++) {
@@ -146,7 +170,7 @@ function arrayCheckFamily () {
 }
 //arrayCheckFamily();
 
-
+//if something is wrong the number will change to red, this lets it change back to compare again
 function changeAllToBlue() {
   for (let i=0; i<grid[0].length; i++) {
     for (let j=0; j<grid[0].length; j++) {
@@ -155,7 +179,38 @@ function changeAllToBlue() {
   }
 }
 
+// ERASE THIS IT DOES NOTHING
+function changeDifficulty (challengeRating) {
+  if (challengeRating === 1) {
+    changePuzzle(1)
+  } else if (challengeRating === 2) {
+    changePuzzle(2)
+  } else if (challengeRating === 3) {
+    changePuzzle(3)
+  } else {
+    console.log("error in changeDifficulty")
+  }
+}
 
+
+//makes sure everything entered is a number, if it is, it will run the checkAll
+function checkIfNumber () {
+  let allAreNumberCheck = 0
+  for (let i=0; i<grid[0].length; i++) {
+    for (let j=0; j<grid[0].length; j++) {
+      if (grid[i][j]===NaN) {
+        alert("Please enter a valid number into every spot before submiting") 
+      } else {
+        allAreNumberCheck++
+      }
+    }   
+  }
+  if (allAreNumberCheck===324) {
+    checkAll()
+  }
+}
+
+//runs every check and resets the colors to blue. if everything is right than you win!
 function checkAll () {
   wrongAnswer = 0;
   changeAllToBlue();
@@ -167,10 +222,14 @@ function checkAll () {
   }
 }
 
+//TODO
+//
+//want to change the css of wrong ones, need to also change that it is disabled
+//add a way to grab preset sudoku puzzles from an api
 
-//Could make easier if i could find a way to family + i into calling variable name
-//want to change the css of wrong ones,
-//Hardcoded arrays I replaced but wanted to save just in case
+
+
+//Hardcoded arrays I replaced with calculateFamilies but wanted to save just in case
  /*
 const family1 = [
   grid[0][0], grid[0][1], grid[0][2],
